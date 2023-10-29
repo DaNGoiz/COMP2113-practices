9740a2df4278a3a90c6eb7cefa506d7d5d856d96
Izzy <3332099165@qq.com>
2023-10-29 08:14:07 +0000

    Add Ass2 update

---
+++ Assignment2/Problem1/1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+    int num;
+    cin >> num;
+
+    if (num <= 0) return 0;
+
+    vector<int> pairs(num, 0);
+
+    int numOfPairs = 0;
+    int sum = 0;
+
+    for (int i = 0; i < num; ++i) {
+        string input;
+        int cardNum;
+
+        cin >> input;
+        cardNum = stoi(input.substr(1));
+
+        sum += cardNum;
+
+        bool needToAdd = false;
+        for (int j = 0; j < num; ++j) {
+            if (pairs[j] == cardNum) {
+                numOfPairs ++;
+                pairs[j] = 0;
+                needToAdd = false;
+                break;
+            }
+            needToAdd = true;
+        }
+
+        if (needToAdd) {
+            for (int j = 0; j < num; ++j) {
+                if (pairs[j] == 0) {
+                    pairs[j] = cardNum;
+                    break;
+                }
+            }
+        }
+
+        // for (int k = 0; k < pairs.size(); k++) {
+        //     cout << pairs[k] << " ";
+        // }
+        // cout << numOfPairs << endl;
+    }
+
+    cout << numOfPairs << endl << sum << endl;
+
+    return 0;
+}
