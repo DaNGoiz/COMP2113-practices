039ac93644f2e667002fc9eb3d37294471e4eb6e
Izzy <3332099165@qq.com>
2023-11-17 15:38:51 +0000

    Update C10_2

--- Module10/C10_1.c
+++ Module10/C10_1.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
-int main () {
-    printf("Hello world!\n");
+
+int main() {
+    int total_seats = 6;
+    int total_votes = 900000;
+    int num_lists = 8;
+    int votes[] = {80000, 120000, 400000, 60000, 6000, 180000, 34000, 20000};
+    int quota = total_votes / total_seats;
+    int automatic_seats[num_lists];
+    int remainders[num_lists];
+
+    printf("Total number of seats: %d\n", total_seats);
+    printf("Total number of votes: %d\n", total_votes);
+    printf("Total number of lists: %d\n", num_lists);
+
+    for(int i = 0; i < num_lists; i++) {
+        automatic_seats[i] = votes[i] / quota; // Calculate automatic seats
+        remainders[i] = votes[i] % quota;      // Calculate remainders
+        printf("Votes for list %d: %d\n", i + 1, votes[i]);
+        printf("Automatic seat for list %d: %d\n", i + 1, automatic_seats[i]);
+        printf("Remainder for list %d: %d\n", i + 1, remainders[i]);
+    }
+
     return 0;
 }
